07f6dd57dc7aeb14f30d8a296e901179c18d05a3
jvviel <p-vittor@hotmail.com>
2018-06-07 14:34:33 -0300

    funcionalidade

---
+++ src/viel/victor/joao/Crypt.java
@@ -0,0 +1,62 @@
+package viel.victor.joao;
+
+public class Crypt {
+
+	private static final String pwd = "mandioca";
+
+    public String CryptHexa(String chave) {
+        String cript = "";
+        String b = "";
+        String p = "";
+        int j = 0;
+        int a1 = 0;
+        int a2 = 0;
+        int a3 = 0;
+
+        for (int i = 0; i < pwd.length(); i++) {
+            p += (int) pwd.charAt(i);
+        }
+        for (int i = 0; i < chave.length(); i++) {
+            a1 = (int) p.charAt(j);
+            j++;
+            if (j == p.length()) {
+                j = 0;
+            }
+            a2 = (int) chave.charAt(i);
+            a3 = a1 ^ a2;
+            b = Integer.toHexString(a3);
+            if (b.length() < 2) {
+                b = "0" + b;
+            }
+            cript += b;
+        }
+        return cript;
+    }
+
+    public String DeCryptHexa(String chave) {
+        String decript = "";
+        String b = "";
+        String p = "";
+        int j = 0;
+        int a1 = 0;
+        int a2 = 0;
+        int a3 = 0;
+
+        for (int i = 0; i < pwd.length(); i++) {
+            p += (int) pwd.charAt(i);
+        }
+        for (int i = 0; i < chave.length(); i += 2) {
+            a1 = (int) p.charAt(j);
+            j++;
+            if (j == p.length()) {
+                j = 0;
+            }
+            b = chave.substring(i, i + 2);
+            a3 = Integer.parseInt(b, 16);
+            a2 = a1 ^ a3;
+            decript += (char) a2;
+        }
+        return decript;
+    }
+
+}
---
+++ src/viel/victor/joao/Main.java
@@ -0,0 +1,18 @@
+package viel.victor.joao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Crypt crypt = new Crypt();
+		String encriptar = crypt.CryptHexa("Ola Senhores");
+		System.out.println(encriptar);
+		
+		String decriptar = crypt.DeCryptHexa(encriptar);
+		System.out.println(decriptar);
+		
+		//   Resultado =>
+		//   7e5c581964545f585e425542
+		//   Ola Senhores
+	}
+}
